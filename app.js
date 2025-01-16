9f6c252fce8190937dbfb2b73ad452b0f5036723
vasusaini0001 <vasu.saini@kalvium.community>
2025-01-16 12:42:35 +0530

    test deploy api

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+
+module.exports = config;
+
+
+console.log(config);
